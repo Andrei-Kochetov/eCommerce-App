139f137d709ad0a434dba879cbcb72eaa863ffcd
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-08-14 13:32:01 +0300

    feat: add routes module

--- src/spa/app.ts
+++ src/spa/app.ts
@@ -1,8 +1,7 @@
 import '@src/spa/common.scss';
 import { IApp } from '@src/spa/types';
 import { IBasePage } from '@src/spa/view/pages/basePage/types';
-import BasePageView from '@src/spa/view/pages/basePage/basePageView';
-import { ELEMENT_PAGE_NAME_ATTRIBUTE } from '@src/spa/view/pages/types';
+import BasePage from '@src/spa/view/pages/basePage/basePageView';
 
 // The main class of the application that provides app functionality to the entry point - index.ts
 export default class App {
@@ -12,7 +11,7 @@ export default class App {
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   private constructor() {
-    this.basePage = new BasePageView();
+    this.basePage = new BasePage();
   }
 
   public static getInstance(): IApp {
@@ -21,25 +20,25 @@ export default class App {
 
   public start(): void {
     this.basePage.startRendering();
-    this.navigate();
+    // this.navigate();
   }
 
   // temporary method for initial navigation, in the future will be replaced by router
-  private navigate(): void {
-    const header: HTMLElement = this.basePage.getHeader().getView();
-    header.addEventListener('click', (event: MouseEvent): void => {
-      if (!(event.target instanceof HTMLElement)) return;
-      const pageName: string | undefined = event.target.dataset[ELEMENT_PAGE_NAME_ATTRIBUTE];
-      if (!pageName) return;
-      this.basePage.renderPage(pageName);
-    });
+  // private navigate(): void {
+  //   const header: HTMLElement = this.basePage.getHeader().getView();
+  //   header.addEventListener('click', (event: MouseEvent): void => {
+  //     if (!(event.target instanceof HTMLElement)) return;
+  //     const pageName: string | undefined = event.target.dataset[ELEMENT_PAGE_NAME_ATTRIBUTE];
+  //     if (!pageName) return;
+  //     this.basePage.renderPage(pageName);
+  //   });
 
-    const main: HTMLElement = this.basePage.getMain().getView();
-    main.addEventListener('click', (event: MouseEvent): void => {
-      if (!(event.target instanceof HTMLElement)) return;
-      const pageName: string | undefined = event.target.dataset[ELEMENT_PAGE_NAME_ATTRIBUTE];
-      if (!pageName) return;
-      this.basePage.renderPage(pageName);
-    });
-  }
+  //   const main: HTMLElement = this.basePage.getMain().getView();
+  //   main.addEventListener('click', (event: MouseEvent): void => {
+  //     if (!(event.target instanceof HTMLElement)) return;
+  //     const pageName: string | undefined = event.target.dataset[ELEMENT_PAGE_NAME_ATTRIBUTE];
+  //     if (!pageName) return;
+  //     this.basePage.renderPage(pageName);
+  //   });
+  // }
 }
--- src/spa/view/pages/basePage/basePageView.ts
+++ src/spa/view/pages/basePage/basePageView.ts
@@ -2,22 +2,17 @@ import { IHeader } from '@src/spa/view/header/types';
 import HeaderView from '@src/spa/view/header/headerView';
 import IView from '@src/spa/view/types';
 import FooterView from '@src/spa/view/footer/footerView';
-import { PageNames } from '@src/spa/view/pages/types';
 import LoginPageView from '@src/spa/view/pages/loginPage/loginPageView';
 import { IMain } from '@src/spa/view/main/types';
 import MainView from '@src/spa/view/main/mainView';
-import HomePageView from '@src/spa/view/pages/homePage/homePageView';
-import NotFoundPageView from '@src/spa/view/pages/notFoundPage/notFoundPageView';
 import RegistrationPageView from '@src/spa/view/pages/registrationPage/registrationPageView';
 import { APP_STATE_KEYS, IState } from '@src/spa/logic/state/types';
 import State from '@src/spa/logic/state/state';
+import { IBasePage } from '@src/spa/view/pages/basePage/types';
 
-export default class IBasePage {
-  private readonly defaultPage: string = PageNames.MAIN;
+export default class BasePage implements IBasePage {
   private readonly header: IHeader;
   private readonly main: IMain;
-  private readonly pages: Map<string, IView> = new Map();
-  private currentPage = '';
 
   public constructor() {
     this.header = new HeaderView();
@@ -32,27 +27,13 @@ export default class IBasePage {
     return this.main;
   }
 
-  public getCurrentPage(): IView {
-    const page: IView | undefined = this.pages.get(this.currentPage);
-    if (!page) throw new Error('App Error! Current page is missing!');
-    return page;
-  }
-
   public startRendering(): void {
     const footer: IView = new FooterView();
     document.body.append(this.header.getView(), this.main.getView(), footer.getView());
-    this.renderPage(this.defaultPage);
   }
 
-  public renderPage(pageName: string): void {
+  public renderPage(page: IView): void {
     const state: IState = State.getInstance();
-    let page: IView | undefined = this.pages.get(pageName);
-    if (pageName === this.currentPage) return; // blocking rendering same page
-    this.currentPage = pageName;
-
-    if (!page) {
-      page = this.getPage(pageName);
-    }
 
     if (page instanceof LoginPageView || page instanceof RegistrationPageView) {
       state.setRecord(APP_STATE_KEYS.IS_SPECIAL_PAGE, 'true');
@@ -64,48 +45,48 @@ export default class IBasePage {
     this.main.addPage(page);
   }
 
-  private getPage(pageName: string): IView {
-    let page: IView;
-    switch (pageName) {
-      case PageNames.MAIN:
-        page = new HomePageView();
-        this.pages.set(PageNames.MAIN, page);
-        return page;
-      case PageNames.LOGIN:
-        page = new LoginPageView();
-        this.pages.set(PageNames.LOGIN, page);
-        return page;
-      case PageNames.REGISTRATION:
-        page = new RegistrationPageView();
-        this.pages.set(PageNames.REGISTRATION, page);
-        return page;
-      case PageNames.CATALOG:
-        page = this.getNotFoundPage(); // later change on catalog page
-        this.pages.set(PageNames.CATALOG, page);
-        return page;
-      case PageNames.PROFILE:
-        page = this.getNotFoundPage(); // later change on PROFILE page
-        this.pages.set(PageNames.PROFILE, page);
-        return page;
-      case PageNames.BASKET:
-        page = this.getNotFoundPage(); // later change on BASKET page
-        this.pages.set(PageNames.BASKET, page);
-        return page;
-      case PageNames.ABOUT_US:
-        page = this.getNotFoundPage(); // later change on ABOUT_US page
-        this.pages.set(PageNames.ABOUT_US, page);
-        return page;
-      default:
-        return this.getNotFoundPage();
-    }
-  }
+  // private getPage(pageName: string): IView {
+  //   let page: IView;
+  //   switch (pageName) {
+  //     case PageNames.MAIN:
+  //       page = new HomePageView();
+  //       this.pages.set(PageNames.MAIN, page);
+  //       return page;
+  //     case PageNames.LOGIN:
+  //       page = new LoginPageView();
+  //       this.pages.set(PageNames.LOGIN, page);
+  //       return page;
+  //     case PageNames.REGISTRATION:
+  //       page = new RegistrationPageView();
+  //       this.pages.set(PageNames.REGISTRATION, page);
+  //       return page;
+  //     case PageNames.CATALOG:
+  //       page = this.getNotFoundPage(); // later change on catalog page
+  //       this.pages.set(PageNames.CATALOG, page);
+  //       return page;
+  //     case PageNames.PROFILE:
+  //       page = this.getNotFoundPage(); // later change on PROFILE page
+  //       this.pages.set(PageNames.PROFILE, page);
+  //       return page;
+  //     case PageNames.BASKET:
+  //       page = this.getNotFoundPage(); // later change on BASKET page
+  //       this.pages.set(PageNames.BASKET, page);
+  //       return page;
+  //     case PageNames.ABOUT_US:
+  //       page = this.getNotFoundPage(); // later change on ABOUT_US page
+  //       this.pages.set(PageNames.ABOUT_US, page);
+  //       return page;
+  //     default:
+  //       return this.getNotFoundPage();
+  //   }
+  // }
 
-  private getNotFoundPage(): IView {
-    let page: IView | undefined = this.pages.get(PageNames.NOT_FOUND);
-    if (!page) {
-      page = new NotFoundPageView();
-      this.pages.set(PageNames.NOT_FOUND, page);
-    }
-    return page;
-  }
+  // private getNotFoundPage(): IView {
+  //   let page: IView | undefined = this.pages.get(PageNames.NOT_FOUND);
+  //   if (!page) {
+  //     page = new NotFoundPageView();
+  //     this.pages.set(PageNames.NOT_FOUND, page);
+  //   }
+  //   return page;
+  // }
 }
