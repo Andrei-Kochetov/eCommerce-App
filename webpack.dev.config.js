8ebac4ad5bb272c046e8a037dfefaad390c3035d
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-08-15 14:46:19 +0300

    refactor: change devServer config to make possible enter path in the browser search bar

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, '../dist'),
-    },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 4200,
+    hot: true,
+    contentBase: path.resolve(__dirname, '../dist'),
+    historyApiFallback: true,
+  },
 };
