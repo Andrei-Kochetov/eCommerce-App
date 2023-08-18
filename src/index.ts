ba494bd0246243891f113ee554f73433da4cfdae
Andrei <ai.dr@yandex.ru>
2023-08-18 12:45:23 +0400

    added a test client, setting up the environment to work with the SDK

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import App from '@src/spa/app';
 import { IApp } from '@src/spa/types';
+import { getProject } from '@src/spa/model/clientApi/CreateClient';
 
 // creating entry point for app launching
 const app: IApp = App.getInstance();
 app.start();
+getProject().then(console.log).catch(console.error);
