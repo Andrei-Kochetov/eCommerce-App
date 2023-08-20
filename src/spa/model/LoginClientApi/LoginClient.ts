489adf06e8c2aedcae07379e8f030be5f8bb7c78
Andrei <ai.dr@yandex.ru>
2023-08-20 16:27:13 +0400

    feat: add token in login client

--- src/spa/model/LoginClientApi/LoginClient.ts
+++ src/spa/model/LoginClientApi/LoginClient.ts
@@ -17,7 +17,7 @@ export default class LoginClient implements ILoginClient {
   public authorization = (email: string, password: string) => {
     const passwordMiddlewareOptions: PasswordAuthMiddlewareOptions = {
       host: 'https://auth.europe-west1.gcp.commercetools.com',
-      projectKey: 'ecommerce-app2023',
+      projectKey: options.projectKey,
       credentials: {
         clientId: '5CtkecLYr54FhhzXfgya3yRC',
         clientSecret: 'Y0wVAYt9uSgpgJFn4_lP9r_mmFlYMlK0',
@@ -27,9 +27,7 @@ export default class LoginClient implements ILoginClient {
         },
       },
       tokenCache: this.token,
-      scopes: [
-        `manage_my_payments:ecommerce-app2023 manage_my_quotes:ecommerce-app2023 view_published_products:ecommerce-app2023 manage_my_profile:ecommerce-app2023 view_categories:ecommerce-app2023 manage_my_quote_requests:ecommerce-app2023 manage_my_business_units:ecommerce-app2023 manage_my_shopping_lists:ecommerce-app2023 create_anonymous_token:ecommerce-app2023 view_project_settings:ecommerce-app2023 manage_my_orders:ecommerce-app2023`,
-      ],
+      scopes: options.scopes,
       fetch,
     };
     const ctpClient = new ClientBuilder()
--- src/spa/model/LoginClientApi/constants.ts
+++ src/spa/model/LoginClientApi/constants.ts
@@ -1,5 +1,8 @@
 export const options = {
   projectKey: 'ecommerce-app2023',
+  scopes: [
+    'manage_my_payments:ecommerce-app2023 manage_my_quotes:ecommerce-app2023 view_published_products:ecommerce-app2023 manage_my_profile:ecommerce-app2023 view_categories:ecommerce-app2023 manage_my_quote_requests:ecommerce-app2023 manage_my_business_units:ecommerce-app2023 manage_my_shopping_lists:ecommerce-app2023 create_anonymous_token:ecommerce-app2023 view_project_settings:ecommerce-app2023 manage_my_orders:ecommerce-app2023',
+  ],
   authMiddlewareOptions: {
     host: 'https://auth.europe-west1.gcp.commercetools.com',
     projectKey: 'ecommerce-app2023',
