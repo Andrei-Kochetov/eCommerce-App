88f7a053dbb456f4fcb7280a88c0567f7c3f4205
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-08-24 12:05:27 +0300

    feat: add token getter method in ProfileDataManager

--- src/spa/logic/profile/profileDataManager/profileDataManager.ts
+++ src/spa/logic/profile/profileDataManager/profileDataManager.ts
@@ -1,10 +1,19 @@
 import { IProfileDataManager, ProfileData } from '@src/spa/logic/profile/profileDataManager/types';
+import { APP_STATE_KEYS, IState } from '@src/spa/logic/state/types';
+import State from '@src/spa/logic/state/state';
+import { TokenCache } from '@commercetools/sdk-client-v2';
 
 export default class ProfileDataManager implements IProfileDataManager {
-  // singleton
+  private static readonly instance: IProfileDataManager = new ProfileDataManager();
+
+  private readonly token: TokenCache;
 
   private constructor() {
-    // this.token = JSON.parse(State[TOKEN]);
+    this.token = this.getToken();
+  }
+
+  public static getInstance(): IProfileDataManager {
+    return this.instance;
   }
 
   public getProfileData(): ProfileData {
@@ -13,4 +22,10 @@ export default class ProfileDataManager implements IProfileDataManager {
   }
 
   // and public methods for password, email, user-info, addresses updating
+
+  private getToken(): TokenCache {
+    const state: IState = State.getInstance();
+    const token = state.getRecord(APP_STATE_KEYS.TOKEN);
+    return JSON.parse(token);
+  }
 }
