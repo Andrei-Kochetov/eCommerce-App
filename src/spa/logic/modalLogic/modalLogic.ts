42c8b1995f64d93518dbf26ad9a5488a71a4364f
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-08-26 22:35:29 +0300

    fix: fix message showing

--- src/spa/logic/modalLogic/modalLogic.ts
+++ src/spa/logic/modalLogic/modalLogic.ts
@@ -1,21 +1,20 @@
 import { IModalLogic } from '@src/spa/logic/modalLogic/types';
+import { IElementCreator } from '@src/spa/utils/elementCreator/types';
+import ButtonView from '@src/spa/view/button/buttonView';
+import { btnParams } from '@src/spa/view/button/types';
 import { IModal } from '@src/spa/view/modal/types';
 import PopUpView from '@src/spa/view/popUp/popUpView';
 import { IPopUpView } from '@src/spa/view/popUp/types';
 
-const ARE_YOU_SURE_MESSAGE = `Addresses data have been changed.
-  Are you sure you want to exit without saving those changes?
-  To EXIT press CANCEL or X at the top once more`;
+const ARE_YOU_SURE_MESSAGE = `You want to exit without saving changes?`;
+const YES_BTN_CLASS = 'btn_yes';
 
 export default abstract class ModalLogic<T extends IModal> implements IModalLogic {
-  private areYouSure: boolean;
-
   protected wereChanges: boolean;
 
   protected readonly modal: T;
 
   public constructor(modal: T) {
-    this.areYouSure = false;
     this.wereChanges = false;
     this.modal = modal;
   }
@@ -30,13 +29,29 @@ export default abstract class ModalLogic<T extends IModal> implements IModalLogi
   }
 
   public exitHandler(): void {
-    if (this.wereChanges && !this.areYouSure) {
-      this.areYouSure = true;
+    if (this.wereChanges) {
       const popUp: IPopUpView = PopUpView.getRejectPopUpWithoutImg(ARE_YOU_SURE_MESSAGE);
+      popUp.getView().prepend(this.createYesBTN(popUp).getElement());
       popUp.showWithoutAutoHiding();
       return;
     } else {
       this.modal.hideModal();
     }
+  }
+
+  private createYesBTN(popUp: IPopUpView): IElementCreator {
+    const params: btnParams = {
+      textContent: 'Yes',
+      classNames: [YES_BTN_CLASS],
+    };
+    const button: IElementCreator = new ButtonView(params).getViewCreator();
+    button.setListeners({
+      event: 'click',
+      callback: (): void => {
+        this.modal.hideModal();
+        popUp.getView().remove();
+      },
+    });
+    return button;
   }
 }
--- src/spa/view/popUp/types.ts
+++ src/spa/view/popUp/types.ts
@@ -1,6 +1,9 @@
+import { IElementCreator } from '@src/spa/utils/elementCreator/types';
 import IView from '@src/spa/view/types';
 
 export interface IPopUpView {
+  getView(): HTMLElement;
+  getViewCreator(): IElementCreator;
   show(): void;
   showWithoutAutoHiding(): void;
 }
