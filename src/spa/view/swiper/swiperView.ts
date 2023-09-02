affbe985459575097a20a7ac899a1ad186ade16a
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-09-03 00:47:37 +0300

    feat: add product page view

--- src/spa/view/swiper/swiperView.ts
+++ src/spa/view/swiper/swiperView.ts
@@ -5,34 +5,59 @@ import { ISwiperView, swiperImgCallback } from '@src/spa/view/swiper/types';
 
 // Import Swiper and modules
 import Swiper from 'swiper';
-import { Mousewheel, Navigation, Pagination, Parallax, Scrollbar } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 // import Swiper and modules styles
 import '@node_modules/swiper/swiper.scss';
-import '@node_modules/swiper/modules/scrollbar.scss';
 import '@node_modules/swiper/modules/navigation.scss';
 import '@node_modules/swiper/modules/pagination.scss';
 
 export default class SwiperView implements ISwiperView {
-  private readonly layout: HTMLElement;
-  private readonly btnPrev: HTMLElement;
-  private readonly btnNext: HTMLElement;
-  private readonly pagination: HTMLElement;
-  private readonly scroll: HTMLElement;
-  private readonly wrapper: HTMLElement;
+  private readonly layout: IElementCreator;
+  private readonly btnPrev: IElementCreator;
+  private readonly btnNext: IElementCreator;
+  private readonly pagination: IElementCreator;
+  private readonly wrapper: IElementCreator;
   private readonly swiperOBJ: Swiper;
 
   public constructor(imgURLs: string[], imgCallback?: swiperImgCallback) {
-    this.btnPrev = this.createDivElement(constants.SWIPER_PREV_BTN_CLASS).getElement();
-    this.btnNext = this.createDivElement(constants.SWIPER_NEXT_BTN_CLASS).getElement();
-    this.pagination = this.createDivElement(constants.SWIPER_PAGINATION_CLASS).getElement();
-    this.scroll = this.createDivElement(constants.SWIPER_SCROLLBAR_CLASS).getElement();
-    this.wrapper = this.createDivElement(constants.SWIPER_WRAPPER_CLASS).getElement();
+    this.btnPrev = SwiperView.createDivElement(constants.SWIPER_PREV_BTN_CLASS);
+    this.btnNext = SwiperView.createDivElement(constants.SWIPER_NEXT_BTN_CLASS);
+    this.pagination = SwiperView.createDivElement(constants.SWIPER_PAGINATION_CLASS);
+    this.wrapper = SwiperView.createDivElement(constants.SWIPER_WRAPPER_CLASS);
     this.layout = this.createSwiperHTML(imgURLs, imgCallback);
     this.swiperOBJ = this.createSwiperOBJ();
   }
 
+  public static createDivElement(className: string): IElementCreator {
+    const params: ElementCreatorParams = {
+      tag: constants.DIV_ELEMENT_TAG,
+      classNames: [className],
+    };
+    return new ElementCreator(params);
+  }
+
   public getSwiper(): Swiper {
     return this.swiperOBJ;
+  }
+
+  public getSwiperLayout(): IElementCreator {
+    return this.layout;
+  }
+
+  public getBTNPrev(): IElementCreator {
+    return this.btnPrev;
+  }
+
+  public getBTNNext(): IElementCreator {
+    return this.btnNext;
+  }
+
+  public getPagination(): IElementCreator {
+    return this.pagination;
+  }
+
+  public getWrapper(): IElementCreator {
+    return this.wrapper;
   }
 
   public hideNavigation(): void {
@@ -41,19 +66,19 @@ export default class SwiperView implements ISwiperView {
     this.swiperOBJ.scrollbar.destroy();
   }
 
-  private createSwiperHTML(imgURLs: string[], imgCallback?: swiperImgCallback): HTMLElement {
-    const swiper: IElementCreator = this.createDivElement(constants.SWIPER_CLASS);
+  private createSwiperHTML(imgURLs: string[], imgCallback?: swiperImgCallback): IElementCreator {
+    const swiper: IElementCreator = SwiperView.createDivElement(constants.SWIPER_CLASS);
 
     imgURLs.forEach((url: string): void => {
-      const slide: IElementCreator = this.createDivElement(constants.SWIPER_SLIDE_CLASS);
+      const slide: IElementCreator = SwiperView.createDivElement(constants.SWIPER_SLIDE_CLASS);
       const img: IElementCreator = this.createImg(url, imgCallback);
 
       slide.addInnerElement(img);
-      this.wrapper.append(slide.getElement());
+      this.wrapper.addInnerElement(slide);
     });
-    swiper.addInnerElement(this.wrapper, this.pagination, this.btnPrev, this.btnNext, this.scroll);
+    swiper.addInnerElement(this.wrapper, this.pagination, this.btnPrev, this.btnNext);
 
-    return swiper.getElement();
+    return swiper;
   }
 
   private createImg(url: string, imgCallback?: swiperImgCallback): IElementCreator {
@@ -72,45 +97,27 @@ export default class SwiperView implements ISwiperView {
     return img;
   }
 
-  private createDivElement(className: string): IElementCreator {
-    const params: ElementCreatorParams = {
-      tag: constants.DIV_ELEMENT_TAG,
-      classNames: [className],
-    };
-    return new ElementCreator(params);
-  }
-
   private createSwiperOBJ(): Swiper {
-    const swiper = new Swiper(this.layout, {
+    const swiper = new Swiper(this.layout.getElement(), {
       // configure Swiper to use modules
-      modules: [Navigation, Pagination, Scrollbar, Mousewheel, Parallax],
+      modules: [Navigation, Pagination],
 
       // Optional parameters
       direction: 'horizontal',
       loop: true,
       grabCursor: true,
-      speed: 600,
+      speed: 500,
 
       // pagination
       pagination: {
-        el: this.pagination,
+        el: this.pagination.getElement(),
         clickable: true,
       },
 
       // Navigation arrows
       navigation: {
-        nextEl: this.btnNext,
-        prevEl: this.btnPrev,
-      },
-
-      mousewheel: true,
-      parallax: true,
-
-      //scrollbar
-      scrollbar: {
-        el: this.scroll,
-        draggable: true,
-        snapOnRelease: true,
+        nextEl: this.btnNext.getElement(),
+        prevEl: this.btnPrev.getElement(),
       },
     });
     return swiper;
--- src/spa/view/swiper/types.ts
+++ src/spa/view/swiper/types.ts
@@ -1,3 +1,4 @@
+import { IElementCreator } from '@src/spa/utils/elementCreator/types';
 import Swiper from 'swiper';
 
 export type swiperImgCallback = () => void;
@@ -5,4 +6,9 @@ export type swiperImgCallback = () => void;
 export interface ISwiperView {
   getSwiper(): Swiper;
   hideNavigation(): void;
+  getSwiperLayout(): IElementCreator;
+  getBTNPrev(): IElementCreator;
+  getBTNNext(): IElementCreator;
+  getPagination(): IElementCreator;
+  getWrapper(): IElementCreator;
 }
