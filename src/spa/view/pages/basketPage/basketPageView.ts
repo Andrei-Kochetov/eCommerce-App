387781b6050887665d09d138f24f2d45ac7dc6e8
LeanidRymkevich <leanidrymkevich@gmail.com>
2023-09-09 15:27:52 +0300

    feat: add empty basket view

--- src/spa/view/pages/basketPage/basketPageView.ts
+++ src/spa/view/pages/basketPage/basketPageView.ts
@@ -3,12 +3,74 @@ import PageView from '@src/spa/view/pages/pageView';
 import { PageNames } from '@src/spa/view/pages/types';
 import { CustomBasketData } from '@src/spa/view/pages/basketPage/types';
 import * as constants from '@src/spa/view/pages/basketPage/constants';
+import { IElementCreator } from '@src/spa/utils/elementCreator/types';
+import SwiperView from '@src/spa/view/swiper/swiperView';
+import ElementCreator from '@src/spa/utils/elementCreator/elementCreator';
 
 export default class BasketPageView extends PageView {
   private readonly data: CustomBasketData;
+  private readonly promoCodeInput: HTMLInputElement;
+  private readonly contentWrapper: IElementCreator;
 
   public constructor(data: CustomBasketData) {
-    super(PageNames.CATALOG, constants.BASKET_PAGE_CLASS);
+    super(PageNames.BASKET, constants.BASKET_PAGE_CLASS);
     this.data = data;
+    this.promoCodeInput = BasketPageView.createTextInput(constants.PROMO_CODE_INPUT_CLASS);
+    this.contentWrapper = SwiperView.createDivElement(constants.BASKET_CONTENT_WRAPPER_CLASS);
+    this.configureView(data);
+  }
+
+  public static createTextInput(...classes: string[]): HTMLInputElement {
+    const element: HTMLInputElement = document.createElement('input');
+    element.setAttribute('type', 'text');
+    element.classList.add(...classes);
+
+    return element;
+  }
+
+  private configureView(data: CustomBasketData): void {
+    const header: IElementCreator = this.createPageHeader();
+
+    if (data.products.length === 0) {
+      this.contentWrapper.addInnerElement(this.createEmptyBasketView());
+    } else {
+      console.log(data.products);
+    }
+
+    this.getViewCreator().addInnerElement(header, this.contentWrapper);
+  }
+
+  private createEmptyBasketView(): IElementCreator {
+    const emptyBasket: IElementCreator = SwiperView.createDivElement(constants.EMPTY_BASKET_CLASS);
+    const img: IElementCreator = new ElementCreator(constants.EMPTY_BASKET_IMG_PARAMS);
+    const title: IElementCreator = new ElementCreator(constants.EMPTY_BASKET_TITLE_PARAMS);
+    const span_1: IElementCreator = new ElementCreator(constants.EMPTY_BASKET_TEXT_1_PARAMS);
+    const span_2: IElementCreator = new ElementCreator(constants.EMPTY_BASKET_TEXT_2_PARAMS);
+    const link: IElementCreator = new ElementCreator(constants.EMPTY_BASKET_LINK_PARAMS);
+    const textWrapper: IElementCreator = new ElementCreator(constants.EMPTY_BASKET_TEXT_WRAPPER_PARAMS);
+
+    textWrapper.addInnerElement(span_1, link, span_2);
+    emptyBasket.addInnerElement(img, title, textWrapper);
+
+    return emptyBasket;
+  }
+
+  private createPageHeader(): IElementCreator {
+    const header: IElementCreator = SwiperView.createDivElement(constants.PAGE_HEADER_CLASS);
+    const title: IElementCreator = new ElementCreator(constants.PAGE_TITLE_PARAMS);
+    const promoCode: IElementCreator = this.createPromoCodeElement();
+
+    header.addInnerElement(title, promoCode);
+    return header;
+  }
+
+  private createPromoCodeElement(): IElementCreator {
+    const promoCode: IElementCreator = SwiperView.createDivElement(constants.PROMO_CODE_CLASS);
+    const promoCodeImg: IElementCreator = new ElementCreator(constants.PROMO_CODE_IMG_PARAMS);
+
+    this.promoCodeInput.setAttribute('placeholder', constants.PROMO_CODE_INPUT_PLACEHOLDER);
+    promoCode.addInnerElement(promoCodeImg, this.promoCodeInput);
+
+    return promoCode;
   }
 }
--- src/spa/view/pages/basketPage/constants.ts
+++ src/spa/view/pages/basketPage/constants.ts
@@ -1,1 +1,59 @@
+import { ElementCreatorParams } from '@src/spa/utils/elementCreator/types';
+import promoCodeImgSRC from '@src/assets/basket-page/promo-code.png';
+import emptyBasketImgSRC from '@src/assets/basket-page/empty-cart.png';
+
 export const BASKET_PAGE_CLASS = 'basket';
+
+export const PAGE_HEADER_CLASS = 'basket__header';
+
+export const PAGE_TITLE_PARAMS: ElementCreatorParams = {
+  tag: 'h2',
+  classNames: ['basket__title'],
+  textContent: 'Basket',
+};
+
+// promo code element
+export const PROMO_CODE_CLASS = 'promo-code';
+export const PROMO_CODE_INPUT_CLASS = 'promo-code__input';
+export const PROMO_CODE_INPUT_PLACEHOLDER = 'Promo Code';
+export const PROMO_CODE_IMG_PARAMS: ElementCreatorParams = {
+  tag: 'img',
+  classNames: ['promo-code__img'],
+  attributes: {
+    src: promoCodeImgSRC,
+    alt: 'promo-code__img',
+  },
+};
+
+export const BASKET_CONTENT_WRAPPER_CLASS = 'basket__content-wrapper';
+export const EMPTY_BASKET_CLASS = 'basket__empty-info';
+export const EMPTY_BASKET_IMG_PARAMS: ElementCreatorParams = {
+  tag: 'img',
+  classNames: ['basket__empty-info-img'],
+  attributes: {
+    src: emptyBasketImgSRC,
+    alt: 'basket__empty-info-img',
+  },
+};
+export const EMPTY_BASKET_TITLE_PARAMS: ElementCreatorParams = {
+  tag: 'h3',
+  classNames: ['basket__empty-info-title'],
+  textContent: 'Your basket is empty',
+};
+export const EMPTY_BASKET_TEXT_1_PARAMS: ElementCreatorParams = {
+  tag: 'span',
+  textContent: 'Go to the ',
+};
+export const EMPTY_BASKET_TEXT_2_PARAMS: ElementCreatorParams = {
+  tag: 'span',
+  textContent: ' and add products',
+};
+export const EMPTY_BASKET_LINK_PARAMS: ElementCreatorParams = {
+  tag: 'a',
+  textContent: 'catalog',
+  attributes: { href: '/catalog' },
+};
+export const EMPTY_BASKET_TEXT_WRAPPER_PARAMS: ElementCreatorParams = {
+  tag: 'p',
+  classNames: ['basket__empty-info-text-wrapper'],
+};
